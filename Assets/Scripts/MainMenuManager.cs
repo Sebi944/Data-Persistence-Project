60de783bb8338d4a8303dc20650fdc0ce58529c0
Sebi944 <151718919+Sebi944@users.noreply.github.com>
2024-10-04 19:28:38 +0300

    MainMenuScene.v0.2

---
+++ Assets/Scripts/MainMenuManager.cs
@@ -0,0 +1,37 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+#if UNITY_EDITOR
+using UnityEditor;
+#endif
+
+public class MainMenuManager : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void Exit()
+    {
+#if UNITY_EDITOR
+        EditorApplication.ExitPlaymode();
+#else
+        Application.Quit(); // original code to quit Unity player
+#endif
+    }
+
+    public void StartNew()
+    {
+        SceneManager.LoadScene(1);
+    }
+}
